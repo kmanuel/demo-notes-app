ca35cb3b34131239916a5c7e4fce1a03180e2e75
kmanuel <kruisz88@gmail.com>
2022-03-28 04:52:33 +0200

    add API

---
+++ src/get.js
@@ -0,0 +1,19 @@
+import dynamoDb from './util/dynamodb'
+import handler from './util/handler'
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      userId: '123',
+      noteId: event.pathParameters.id,
+    },
+  }
+
+  const result = await dynamoDb.get(params)
+  if (!result.Item) {
+    throw new Error('Item not found.')
+  }
+
+  return result.Item
+})
